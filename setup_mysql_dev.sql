2487fbee7c4617d15d335ea80ad3a92af1655d53
vSolomony <mohamed.a.kassem95@gmail.com>
2024-01-17 10:39:30 -0800

    123

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
--- Create the database hbnb_dev_db if it doesn't exist
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user hbnb_dev with password hbnb_dev_pwd if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges to the user hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';

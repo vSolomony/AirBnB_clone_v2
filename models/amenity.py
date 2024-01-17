79aa4eea1f7e7bc1d66f8b42b27a79fe4dd69ef0
vSolomony <mohamed.a.kassem95@gmail.com>
2024-01-17 11:19:51 -0800

    123

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""BaseMODEL CLASS"""
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    """Amenity class"""
-
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Amenity init"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel, Base):
+	"""This is the class for Amenity
+	Attributes:
+		name: input name
+	"""
+	__tablename__ = "amenities"
+	name = Column(String(128), nullable=False)
+	place_amenities = relationship("Place", secondary=place_amenity)
--- models/review.py
+++ models/review.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Base Model Class"""
-from models.base_model import BaseModel
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
-    """The Review class"""
-
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    def __init__(self, *args, **kwargs):
-        """review init"""
-        super().__init__(*args, **kwargs)
-
+class Review(BaseModel, Base):
+	"""This is the class for Review
+	Attributes:
+		place_id: place id
+		user_id: user id
+		text: review description
+	"""
+	__tablename__ = "reviews"
+	text = Column(String(1024), nullable=False)
+	place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+	user_id = Column(String(60), ForeignKey("users.id"), nullable=False)

2e248d24b2fd60e1dddc72ad3d504d4be7bc4a45
vSolomony <mohamed.a.kassem95@gmail.com>
2024-01-17 11:23:22 -0800

    123

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""file storage for application"""
-from models.engine.file_storage import FileStorage
-
-"""instance of file storage"""
-storage = FileStorage()
-storage.reload()
-
